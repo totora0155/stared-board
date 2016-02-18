18783597e89df620d63fc6eb185599485c2a6ef4
nju33 <nju33@nmba.local>
2016-02-19 03:56:30 +0900

    Add `<Startup/>`

--- src/scripts/components/board.js
+++ src/scripts/components/board.js
@@ -4,8 +4,28 @@ import map from 'lodash.map';
 import BoardList from 'components/board-list';
 import Masonry from 'masonry-layout';
 import Alert from 'components/alert';
+import Startup from 'components/startup';
+import storage from 'modules/storage';
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null,
+    };
+
+    storage.get('user', true)
+      .then((data) => {
+        if (data.user) {
+          this.setState({user: data.user});
+        }
+      });
+  }
+
+  setUser(user) {
+    this.setState({user});
+  }
+
   componentDidMount() {
     const {alert, boardList} = this.refs;
     new Masonry(this.refs.boardList, {
@@ -17,15 +37,25 @@ class Board extends React.Component {
   }
 
   render() {
-    const boardLists = map(this.props.data, (langData) => {
-      return <BoardList key={langData.lang} data={langData}/>
-    });
-    return (
-      <div>
+    console.log(this);
+    let component = null;
+    if (this.state.user) {
+      const boardLists = map(this.props.data, (langData) => {
+        return <BoardList key={langData.lang} data={langData}/>
+      });
+      component = (
         <div>
           <h1>Hi, {this.props.name}!</h1>
           <ul className="board__list" ref="boardList">{boardLists}</ul>
         </div>
+      );
+    } else {
+      component = <Startup/>;
+    }
+
+    return (
+      <div>
+        {component}
         <Alert ref="alert"/>
       </div>
     );
