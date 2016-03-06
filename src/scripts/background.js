eb2bd63c6f60f1c21fd126ba670b3cceb072f8a2
nju33 <nju33@nmba.local>
2016-03-06 19:25:02 +0900

    Focus the board tab if already been open

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -1,5 +1,11 @@
 const pagePath = chrome.extension.getURL('index.html');
 
 chrome.browserAction.onClicked.addListener((tab) => {
-  chrome.tabs.create({url: pagePath});
+  chrome.tabs.query({url: pagePath}, (tabs) => {
+    if (tabs.length) {
+      chrome.tabs.update(tabs[0].id, {active: true});
+    } else {
+      chrome.tabs.create({url: pagePath});
+    }
+  });
 });
